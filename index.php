0947d565d3782e980e275c8f4d0b99a9cf29b0c1
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2021-03-19 14:49:01 +0100

    updated navbar.html / created myJS.js

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
           crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 </head>
 <body class="bg-light">
 <div class="card card-header text-center text-monospace text-success text-uppercase fixed-top font-weight-bolder"
---
+++ myJS.js
@@ -0,0 +1,11 @@
+function errMsg(){
+    let msg = 'Unavailable';
+    alert(msg);
+}
+function workingOn(){
+    let msg = "working on it..";
+    alert(msg);
+}
+//call method
+errMsg();
+workingOn();
