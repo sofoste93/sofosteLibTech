4aa93b4f18b3fdac1c98edf081eef49518429910
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2021-03-19 14:57:57 +0100

    Update myJS.js

--- myJS.js
+++ myJS.js
@@ -6,6 +6,14 @@ function workingOn(){
     let msg = "working on it..";
     alert(msg);
 }
+
+function welcomeMsg(){
+    let msg = "Hello! Welcome.\nClick Ok to continue.";
+    alert(msg);
+}
+welcomeMsg();
+
 //call method
-errMsg();
-workingOn();
+//errMsg();
+
+//workingOn();
